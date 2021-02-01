a1c071544c53ff0591987a7555f56b07de61d797
jegatheesprana <jegatheesprana@users.noreply.github.com>
2021-02-01 21:55:46 +0530

    Final commit

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransactionDAO.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransactionDAO.java
@@ -1,14 +1,21 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
 
@@ -18,17 +25,34 @@ import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
  * transaction logs are stored in a LinkedList in memory.
  */
 public class PersistentTransactionDAO extends SQLiteOpenHelper implements TransactionDAO {
+    public static final String TRANSACTIONS = "transactions";
     private final List<Transaction> transactions;
 
     public PersistentTransactionDAO(Context context) {
-        super(context, "transactions.db", null, 1);
-        transactions = new LinkedList<>();
+        super(context, TRANSACTIONS + ".db", null, 1);
+        transactions = getFromDB();
     }
 
     @Override
     public void logTransaction(Date date, String accountNo, ExpenseType expenseType, double amount) {
         Transaction transaction = new Transaction(date, accountNo, expenseType, amount);
         transactions.add(transaction);
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        SimpleDateFormat dateFormat = new SimpleDateFormat( "dd/MM/yyyy" );
+
+        cv.put("date", dateFormat.format(date));
+        cv.put("accountNo", accountNo);
+        cv.put("expenseType", expenseType.toString());
+        cv.put("amount", amount);
+
+        long insert = db.insert(TRANSACTIONS, null, cv);
+        if( insert == -1) {
+            //return false;
+        }else{
+            //
+        }
     }
 
     @Override
@@ -47,12 +71,49 @@ public class PersistentTransactionDAO extends SQLiteOpenHelper implements Transa
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        String createTableStatement = "CREATE TABLE " + TRANSACTIONS + " (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "date TEXT," +
+                "accountNo STRING," +
+                "expenseType STRING," +
+                "amount REAL)";
+        db.execSQL(createTableStatement);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
+    }
+
+    public List<Transaction> getFromDB () {
+        List<Transaction> transactions = new LinkedList<>();
+        String queryString = "SELECT * FROM " + TRANSACTIONS;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor result = db.rawQuery(queryString, null);
+        if(result.moveToFirst()) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat( "dd/MM/yyyy" );
+            do {
+                Date date = null;
+                try {
+                    date = dateFormat.parse( result.getString(1) );
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                String accountNo = result.getString(2);
+                ExpenseType expenseType = ExpenseType.valueOf( result.getString(3).toUpperCase() );
+                double amount = result.getDouble(4);
+
+                Transaction transaction = new Transaction(date, accountNo, expenseType, amount);
+                transactions.add(transaction);
+
+            } while ( result.moveToNext() );
+        } else {
+
+        }
+        result.close();
+        db.close();
+
+        return transactions;
     }
 }
